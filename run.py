406f323cbfee31e5f7c66226adbbbfa0c323d961
Codie Stephens-Evans <codie18@gmail.com>
2021-07-25 17:40:40 +0000

    Calculate surplus data

--- run.py
+++ run.py
@@ -77,9 +77,16 @@ def calculate_surplus_data(sales_row):
     """
     print("Calculating surplus data...\n")
     stock = SHEET.worksheet('stock').get_all_values()
-    stock_row = stock[-1] 
     # Slices the final item of the list
-    pprint(stock_row)
+    stock_row = stock[-1]
+
+    surplus_data = []
+    for stock, sales in zip(stock_row, sales_row):
+        surplus = int(stock) - sales
+        surplus_data.append(surplus)
+
+    return surplus_data
+
 
 # Common practice to wrap all main function calls of a program in a funciton called main
 def main():
@@ -89,7 +96,7 @@ def main():
     data = get_sales_data()
     sales_data = [int(num) for num in data]
     update_sales_worksheet(sales_data)
-    calculate_surplus_data(sales_data)
+    new_surplus = calculate_surplus_data(sales_data)
 
 # Functions must be called after they have been declared
 print("Welcome to Love Sandwiches Data Automation")
